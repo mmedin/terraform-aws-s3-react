534f4f4559503027046345cd917a9c4f5e9928aa
Miguel Medina <miguel@medina>
2023-05-31 13:24:27 -0300

    Fix /hashicorp/terraform-provider-aws/issues/28353

--- aws-s3-bucket-website.tf
+++ aws-s3-bucket-website.tf
@@ -10,7 +10,27 @@ resource "aws_s3_bucket" "react_bucket" {
   bucket = var.bucket_name
 }
 
+resource "aws_s3_bucket_public_access_block" "react_bucket_block" {
+  bucket = aws_s3_bucket.react_bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_ownership_controls" "react_bucket_own" {
+  bucket = aws_s3_bucket.react_bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
 resource "aws_s3_bucket_acl" "react_bucket_acl" {
+  depends_on = [
+    aws_s3_bucket_public_access_block.react_bucket_block,
+    aws_s3_bucket_ownership_controls.react_bucket_own,
+  ]
   bucket = aws_s3_bucket.react_bucket.id
   acl    = "public-read"
 }
